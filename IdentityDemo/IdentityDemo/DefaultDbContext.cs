83ddb8c3e0a66a6587bbb8084b2b88c82bc1bb57
Hunter1994 <962410314@qq.com>
2017-11-15 18:08:58 +0800

    初始化程序

---
+++ IdentityDemo/IdentityDemo/DefaultDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using IdentityDemo.Models;
+
+namespace IdentityDemo
+{
+    public class DefaultDbContext:IdentityDbContext
+    {
+        public virtual IDbSet<Product> Products { get; set; }
+        public DefaultDbContext() : base("Default")
+        { }
+    }
+}
